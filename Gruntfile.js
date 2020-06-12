3306fd0b0532764cf2ff31fdb83a20de8192b97a
Zerbe, Kristof (Goetzfried AG) <kristof.zerbe@goetzfried-ag.com>
2020-06-12 17:19:41 +0200

    Promise fix

--- Gruntfile.js
+++ Gruntfile.js
@@ -69,7 +69,6 @@ module.exports = function(grunt) {
   // Whenever the "test" task is run, first clean the "tmp" dir, then run this
   // plugin's task(s), then test the result.
   grunt.registerTask('test', ['clean', 'resource2json', 'nodeunit']);
-  //grunt.registerTask('test', ['clean', 'resource2json']);
 
   // By default, lint and run all tests.
   grunt.registerTask('default', ['jshint', 'test']);
--- tasks/resource2json.js
+++ tasks/resource2json.js
@@ -8,7 +8,6 @@
 
 'use strict';
 
-var Gruntfile = require("../Gruntfile");
 var xmlParser = require('xml2js');
 
 module.exports = function(grunt) {
@@ -30,42 +29,49 @@ module.exports = function(grunt) {
       grunt.log.writeln("Read file '" + f.input);
       var xml = grunt.file.read(f.input);
       
-      xmlParser.parseStringPromise(xml, {
-        explicitArray: false
-      }).then(function (result) {
+      xmlParser.parseString(xml, {
+          explicitArray: false
+        }, function(err, result) {
 
-        var json;
-        switch (options.format.toUpperCase()) {
-          case "JQUERY-LANG":
-            grunt.log.writeln("Convert into '" + options.format + "' to file '" + f.output + "'");
-            json = {
-              "token": {}
-            };
-            result.root.data.forEach(function(d) {
-                grunt.log.writeln("Create token '" + d.$.name + "'");
-                json.token[d.$.name] = d.value;
-            });
-            break;
-
-          // Feel free to implement your format here
-          // case "YOUR-FORMAT":
-          //   break;
-
-          default:
-            grunt.log.warn("The format '" + options.format + "' is not implemented");
+          if (err) {
+            grunt.log.warn("Parsing of '" + f.Input + "' failed");
             return false;
-        }
+          }
 
-        grunt.log.writeln("Write " + f.output);
-        grunt.file.write(f.output, JSON.stringify(json));
-        grunt.log.writeln();
-      })
-      .catch(function (err) {
-        grunt.log.warn("Parsing of '" + f.Input + "' failed");
-        return false;
-      });
+          var json;
+          switch (options.format.toUpperCase()) {
+
+            case "JQUERY-LANG":
+              grunt.log.writeln("Convert into '" + options.format + "' to file '" + f.output + "'");
+              json = {
+                "token": {}
+              };
+              result.root.data.forEach(function(d) {
+                  grunt.log.writeln("Create token '" + d.$.name + "'");
+                  json.token[d.$.name] = d.value;
+              });
+              break;
+  
+            // Feel free to implement your format here
+            // case "YOUR-FORMAT":
+            //   break;
+  
+            default:
+              grunt.log.warn("The format '" + options.format + "' is not implemented");
+              return false;
+          }
+  
+          grunt.log.writeln("Write " + f.output);
+          grunt.file.write(f.output, JSON.stringify(json));
+          grunt.log.writeln();  
+        });
 
     });
 
   });
+
+  var ConvertToJqueryLang = function() {
+    return null;
+  };
+
 };
